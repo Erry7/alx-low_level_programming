8d766a0f1b50bf0fa40c8eb40780ba6b3d2d8957
Erry7 <akhibierica@gmail.com>
2022-04-19 01:54:36 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function that adds two intergers together
+ * @a: takes in an interger
+ * @b: takes in an interger
+ * Return: a + b result
+ */
+int addd(int a, int b)
+{
+return (a +b);
+}
